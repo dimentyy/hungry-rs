467d3878ac3db9d399d02f7498e592b13fd85c88
dmitrii <114256092+dimentyy@users.noreply.github.com>
2025-12-12 22:26:04 +0300

    Add a link for mtproto::Side

--- lib/hungry/src/mtproto/mod.rs
+++ lib/hungry/src/mtproto/mod.rs
@@ -19,6 +19,12 @@ pub const DECRYPTED_MESSAGE_HEADER_SIZE: usize = DecryptedMessage::HEADER_LEN
     + 4  // seq_no
     + 4; // message_data_length
 
+/// For MTProto 2.0, the algorithm for computing
+/// aes_key and aes_iv from auth_key and msg_key is
+/// <...> where x = 0 for messages from client to
+/// server and x = 8 for those from server to client.
+///
+/// https://core.telegram.org/mtproto/description#defining-aes-key-and-initialization-vector
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Side {
     Client = 0,
