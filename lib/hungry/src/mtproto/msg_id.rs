5670b3518d6b864f4c333eabd2e860c7961364b7
dmitrii <114256092+dimentyy@users.noreply.github.com>
2025-12-12 22:03:46 +0300

    Add documentation to MsgIds and SeqNos

--- lib/hungry/src/mtproto/msg_id.rs
+++ lib/hungry/src/mtproto/msg_id.rs
@@ -1,42 +1,78 @@
-use std::fmt;
+// STATUS: stable.
 
+use std::{fmt, time};
+
+/// # Message Identifier (msg_id)
+///
+/// A (time-dependent) 64-bit number used uniquely to identify a
+/// message within a session. Client message identifiers are divisible
+/// by 4, server message identifiers modulo 4 yield 1 if the message
+/// is a response to a client message, and 3 otherwise. Client message
+/// identifiers must increase monotonically (within a single session),
+/// the same as server message identifiers, and must approximately equal
+/// unixtime*2^32. This way, a message identifier points to the approximate
+/// moment in time the message was created. A message is rejected over
+/// 300 seconds after it is created or 30 seconds before it is created
+/// (this is needed to protect from replay attacks). In this situation,
+/// it must be re-sent with a different identifier (or placed in a
+/// container with a higher identifier). The identifier of a message
+/// container must be strictly greater than those of its nested messages.
+///
+/// **Important:** to counter replay-attacks the lower 32 bits of
+/// **msg_id** passed by the client must not be empty and must present
+/// a fractional part of the time point when the message was created.
+///
+/// https://core.telegram.org/mtproto/description#message-identifier-msg-id
 #[derive(Default)]
 pub struct MsgIds {
-    previous: u64,
+    last: i64,
 }
 
 impl fmt::Debug for MsgIds {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("MsgIds")
-            .field("previous", &format_args!("{:#016x}", self.previous))
+            .field("previous", &format_args!("{:#016x}", self.last))
             .finish()
     }
 }
 
 impl MsgIds {
     #[inline]
+    #[must_use]
     pub const fn new() -> Self {
-        Self { previous: 0 }
+        Self { last: 0 }
     }
 
     #[inline]
-    pub const fn previous(&self) -> i64 {
-        self.previous as i64
+    #[must_use]
+    pub const fn last(&self) -> i64 {
+        self.last
     }
 
     #[must_use]
-    pub const fn get(&mut self, since_epoch: std::time::Duration) -> i64 {
-        let subsec_nanos = since_epoch.subsec_nanos() as u64;
-        let message_id = since_epoch.as_secs() << 32 | subsec_nanos << 2;
+    pub const fn get(&mut self, unix_time: time::Duration) -> i64 {
+        let secs = unix_time.as_secs() as i64;
+        let subsec_nanos = unix_time.subsec_nanos() as i64;
 
-        if self.previous >= message_id {
-            self.previous += 4;
+        let message_id = secs << 32 | subsec_nanos << 2;
 
-            self.previous as i64
+        if self.last >= message_id {
+            self.last += 4;
+
+            self.last
         } else {
-            self.previous = message_id;
+            self.last = message_id;
 
-            message_id as i64
+            message_id
         }
+    }
+
+    #[must_use]
+    pub fn get_using_system_time(&mut self) -> i64 {
+        let unix_time = time::SystemTime::now()
+            .duration_since(time::UNIX_EPOCH)
+            .expect("system clock time to be after the Unix epoch");
+
+        self.get(unix_time)
     }
 }
--- lib/hungry/src/mtproto/seq_no.rs
+++ lib/hungry/src/mtproto/seq_no.rs
@@ -1,3 +1,27 @@
+// STATUS: stable.
+
+/// # Message Sequence Number (msg_seqno)
+///
+/// A 32-bit number equal to twice the number of [content-related »] messages
+/// created by the sender prior to this message and subsequently incremented
+/// by one if the current message is a content-related message.
+///
+/// The seqno of a content-related message is thus `msg.seqNo = (current_seqno*2)+1`
+/// (and after generating it, the local `current_seqno` counter must be incremented by 1),
+/// the seqno of a non-content related message is `msg.seqNo = (current_seqno*2)`
+/// (`current_seqno` must not be incremented by 1 after generation).
+///
+/// Thus, the content-relatedness of an incoming MTProto message can simply be
+/// determined by checking the value of the least-significant bit of the seqno
+/// of the message (`message.isContentRelated = (message.seqNo & 1) == 1`).
+///
+/// A container is always generated after its entire contents;
+/// therefore, its sequence number is greater than or equal
+/// to the sequence numbers of the messages contained in it.
+///
+/// [content-related »]: https://core.telegram.org/mtproto/description#content-related-message
+///
+/// https://core.telegram.org/mtproto/description#message-sequence-number-msg-seqno
 #[derive(Debug, Default)]
 pub struct SeqNos {
     current: i32,
@@ -5,6 +29,7 @@ pub struct SeqNos {
 
 impl SeqNos {
     #[inline]
+    #[must_use]
     pub const fn new() -> Self {
         Self { current: 0 }
     }
@@ -12,13 +37,13 @@ impl SeqNos {
     #[inline]
     #[must_use]
     pub const fn non_content_related(&self) -> i32 {
-        self.current
+        self.current * 2
     }
 
     #[inline]
     #[must_use]
     pub const fn get_content_related(&mut self) -> i32 {
-        self.current += 2;
-        self.current - 1
+        self.current += 1;
+        (self.current * 2) - 1
     }
 }
