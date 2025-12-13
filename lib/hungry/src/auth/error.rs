88267803f85c0e351290be0e7bd80024eb35a9ac
dmitrii <114256092+dimentyy@users.noreply.github.com>
2025-12-13 18:53:52 +0300

    Export errors

--- lib/hungry/src/auth/error.rs
+++ lib/hungry/src/auth/error.rs
@@ -1,8 +1,12 @@
 use std::fmt;
 
-use crate::tl;
+use crate::{auth, tl};
 
 use tl::Int128;
+
+pub use auth::req_dh_params::ServerDhParamsOkError;
+pub use auth::req_pq_multi::ResPqError;
+pub use auth::set_client_dh_params::DhGenOkError;
 
 #[derive(Debug)]
 pub struct NonceMismatch {
